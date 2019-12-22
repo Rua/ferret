c187f7ff70f2b826119c3830857ef850a01fa5c0
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-12-22 14:01:18 +0100

    Remove dependency on OpenAL (fixes runtime link issue)

--- main/src/audio.rs
+++ main/src/audio.rs
@@ -1,25 +1,11 @@
-use alto::{Alto, Context};
 use std::error::Error;
 
 pub struct Audio {
-	_al_context: Context,
 }
 
 impl Audio {
 	pub fn new() -> Result<Audio, Box<dyn Error>> {
-		let alto_context = Alto::load_default()?;
-
-		// Open OpenAL
-		let al_device = alto_context.open(None)?;
-		let al_context = al_device.new_context(None)?;
-
-		al_context.set_position([0.0, 0.0, 0.0])?;
-		al_context.set_velocity([0.0, 0.0, 0.0])?;
-		al_context.set_orientation(([1.0, 0.0, 0.0], [0.0, 0.0, 1.0]))?;
-
-		Ok(Audio {
-			_al_context: al_context,
-		})
+      Ok(Audio {})
 	}
 }
 
